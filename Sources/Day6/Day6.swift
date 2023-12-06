ff3f82044f36d1bcb74d7459f4fa45363779c1ca
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2023-12-06 07:34:19 -0500

    Solve day 6 part 2

--- Sources/Day6/Day6.swift
+++ Sources/Day6/Day6.swift
@@ -32,6 +32,11 @@ struct Day6: DayCommand {
             productOfWaysToBeatRaces,
             terminator: "\n\n"
         )
+        
+        let longerRace = parsePartTwo(try readLines())
+        printTitle("Part 2", level: .title1)
+        let waysToBeatLongerRace = part2(race: longerRace)
+        print("Number of ways to beat longer race:", waysToBeatLongerRace)
     }
     
     private func parse(_ lines: [String]) -> [RaceRecord] {
@@ -51,21 +56,45 @@ struct Day6: DayCommand {
         })
     }
     
+    private func parsePartTwo(_ lines: [String]) -> RaceRecord {
+        let time = Int(
+            lines[0]
+                .removingPrefix("Time:")
+                .split(separator: " ")
+                .joined()
+        )!
+        let recordDistance = Int(
+            lines[1]
+                .removingPrefix("Distance:")
+                .split(separator: " ")
+                .joined()
+        )!
+        
+        return RaceRecord(time: time, recordDistance: recordDistance)
+    }
+    
     func part1(races: [RaceRecord]) -> Int {
         races.reduce(into: 1, { product, race in
-            let waysToPlay = 0 ... race.time
-            let waysToBeatRecord = waysToPlay.count(where: { timeHoldingButton in
-                let remainingTime = race.time - timeHoldingButton
-                let distanceTraveled = remainingTime * timeHoldingButton
-                return distanceTraveled > race.recordDistance
-            })
-            
+            let waysToBeatRecord = race.waysToBeat()
             product *= waysToBeatRecord
         })
+    }
+    
+    func part2(race: RaceRecord) -> Int {
+        race.waysToBeat()
     }
     
     struct RaceRecord {
         let time: Int
         let recordDistance: Int
+        
+        func waysToBeat() -> Int {
+            let waysToPlay = 0 ... time
+            return waysToPlay.count(where: { timeHoldingButton in
+                let remainingTime = time - timeHoldingButton
+                let distanceTraveled = remainingTime * timeHoldingButton
+                return distanceTraveled > recordDistance
+            })
+        }
     }
 }
