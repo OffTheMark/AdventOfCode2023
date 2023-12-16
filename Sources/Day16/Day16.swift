c1ee819d25159436063e92b3f2376bd5a579b9ef
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2023-12-16 11:45:58 -0500

    Solve day 16 part 2

--- Sources/Day16/Day16.swift
+++ Sources/Day16/Day16.swift
@@ -27,25 +27,94 @@ struct Day16: DayCommand {
         printTitle("Part 1", level: .title1)
         let numberOfEnergizedTiles = part1(grid: grid)
         print("Number of energized tiles:", numberOfEnergizedTiles, terminator: "\n\n")
+        
+        printTitle("Part 2", level: .title1)
+        let maximumNumberOfEnergizedTiles = part2(grid: grid)
+        print("Maximum number of energized tiles:", maximumNumberOfEnergizedTiles)
     }
     
     func part1(grid: Grid) -> Int {
-        var energizedTiles = Set<Point2D>()
+        grid.energizedTiles(forStartingBeam: .init(position: .zero, direction: .right))
+    }
+    
+    func part2(grid: Grid) -> Int {
+        let startingBeams: [Beam] = grid.topPositions().map({ Beam(position: $0, direction: .down) }) +
+            grid.bottomPositions().map({ Beam(position: $0, direction: .up) }) +
+            grid.leftPositions().map({ Beam(position: $0, direction: .right) }) +
+            grid.rightPositions().map({ Beam(position: $0, direction: .left) })
         
-        let start = Beam(position: .zero, direction: .right)
-        var visited = Set<Beam>()
-        var stack: Deque<Beam> = [start]
+        return startingBeams.reduce(into: 0, { maximumEnergizedTiles, start in
+            let energizedTiles = grid.energizedTiles(forStartingBeam: start)
+            maximumEnergizedTiles = max(maximumEnergizedTiles, energizedTiles)
+        })
+    }
+    
+    struct Grid {
+        typealias Mirror = Day16.Mirror
         
-        while let beam = stack.popLast() {
-            visited.insert(beam)
-            energizedTiles.insert(beam.position)
-            
-            for neighbor in grid.neighbors(for: beam) where !visited.contains(neighbor) {
-                stack.append(neighbor)
-            }
+        let mirrorsByPoint: [Point2D: Mirror]
+        let size: Size2D
+        
+        func topPositions() -> [Point2D] {
+            (0 ..< size.width).map({ Point2D(x: $0, y: 0) })
         }
         
-        return energizedTiles.count
+        func bottomPositions() -> [Point2D] {
+            (0 ..< size.width).map({ Point2D(x: $0, y: size.height - 1) })
+        }
+        
+        func leftPositions() -> [Point2D] {
+            (0 ..< size.height).map({ Point2D(x: 0, y: $0) })
+        }
+        
+        func rightPositions() -> [Point2D] {
+            (0 ..< size.height).map({ Point2D(x: size.width - 1, y: $0) })
+        }
+        
+        func energizedTiles(forStartingBeam start: Beam) -> Int {
+            var energizedTiles = Set<Point2D>()
+            
+            var visited = Set<Beam>()
+            var stack: Deque<Beam> = [start]
+            
+            while let beam = stack.popLast() {
+                visited.insert(beam)
+                energizedTiles.insert(beam.position)
+                
+                for neighbor in neighbors(for: beam) where !visited.contains(neighbor) {
+                    stack.append(neighbor)
+                }
+            }
+            
+            return energizedTiles.count
+        }
+        
+        func neighbors(for beam: Beam) -> [Beam] {
+            guard let mirror = mirrorsByPoint[beam.position] else {
+                var result = beam
+                result.position = result.position.applying(beam.direction.translation)
+                
+                return [result].filter({ contains($0.position) })
+            }
+            
+            let directions = mirror.resultingDirections(for: beam.direction)
+            return directions.compactMap({ direction in
+                let result = Beam(
+                    position: beam.position.applying(direction.translation),
+                    direction: direction
+                )
+                
+                guard contains(result.position) else {
+                    return nil
+                }
+                
+                return result
+            })
+        }
+        
+        func contains(_ point: Point2D) -> Bool {
+            (0 ..< size.width).contains(point.x) && (0 ..< size.height).contains(point.y)
+        }
     }
     
     enum Mirror: Character {
@@ -60,7 +129,7 @@ struct Day16: DayCommand {
                  (.vertical, .down):
                 return [direction]
                 
-            case (.vertical, .left), 
+            case (.vertical, .left),
                  (.vertical, .right):
                 return [.up, .down]
                 
@@ -97,7 +166,11 @@ struct Day16: DayCommand {
                 return [.up]
             }
         }
-        
+    }
+    
+    struct Beam: Hashable {
+        var position: Point2D
+        var direction: Direction
     }
     
     enum Direction: Hashable {
@@ -121,45 +194,6 @@ struct Day16: DayCommand {
                 Translation2D(deltaX: -1, deltaY: 0)
             }
         }
-    }
-    
-    struct Grid {
-        typealias Mirror = Day16.Mirror
-        
-        let mirrorsByPoint: [Point2D: Mirror]
-        let size: Size2D
-        
-        func contains(_ point: Point2D) -> Bool {
-            (0 ..< size.width).contains(point.x) && (0 ..< size.height).contains(point.y)
-        }
-        
-        func neighbors(for beam: Beam) -> [Beam] {
-            guard let mirror = mirrorsByPoint[beam.position] else {
-                var result = beam
-                result.position = result.position.applying(beam.direction.translation)
-                
-                return [result].filter({ contains($0.position) })
-            }
-            
-            let directions = mirror.resultingDirections(for: beam.direction)
-            return directions.compactMap({ direction in
-                let result = Beam(
-                    position: beam.position.applying(direction.translation),
-                    direction: direction
-                )
-                
-                guard contains(result.position) else {
-                    return nil
-                }
-                
-                return result
-            })
-        }
-    }
-    
-    struct Beam: Hashable {
-        var position: Point2D
-        var direction: Direction
     }
 }
 
