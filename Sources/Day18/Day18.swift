991be127023dd8ff9821037043b1117169823f1c
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2023-12-17 22:41:16 -0500

    Prepare day 18 target

---
+++ Sources/Day18/Day18.swift
@@ -0,0 +1,30 @@
+//
+//  Day18.swift
+//
+//
+//  Created by Marc-Antoine Malépart on 2023-12-17.
+//
+
+import Foundation
+import AdventOfCodeUtilities
+import ArgumentParser
+
+struct Day18: DayCommand {
+    static var configuration: CommandConfiguration {
+        CommandConfiguration(
+            commandName: "day18",
+            abstract: "Solve day 18 puzzle"
+        )
+    }
+    
+    @Argument(help: "Puzzle input path")
+    var puzzleInputPath: String
+    
+    func run() throws {
+        // TODO
+        
+        printTitle("Part1", level: .title1)
+        
+        // TODO
+    }
+}
